faeda7aa7a78bb35b2a3b46581c3b8eb454fff19
Alex Fernández <alex.fernandez@logtrust.com>
2018-02-19 01:34:51 +0100

    New faster version.

--- bin/multiply-imul.js
+++ bin/multiply-imul.js
@@ -3,7 +3,8 @@
 let total = 0
 
 for (let i = 0; i < 1000000000; i++) {
-	total += Math.imul(i, i)
+	//total += Math.imul(i, i)
+	total += Math.imul(i | 0, i | 0)
 }
 
 console.log('total: %s', total)
